52dac2ec3e73bcc2e897dd0f28afd19145ab53d5
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2026-02-26 00:20:25 +0000

    Update variables.tf

--- modules/users/variables.tf
+++ modules/users/variables.tf
@@ -1,20 +1,30 @@
-variable "project_id" {
+variable "instance_name" {
+  description = "Name of the OpenStack instance"
   type        = string
-  description = "ID of the project to share network with."
 }
 
-variable "description" {
+variable "flavor_name" {
+  description = "OpenStack flavor (instance type) name"
   type        = string
-  description = "Description of the user."
-  default     = "User created by Infrastructure as code tool to create resources in a dedicated project."
 }
 
-variable "roles" {
-  type = list(string)
-  default = [
-    "network_operator",
-    "network_security_operator",
-    "compute_operator",
-    "image_operator",
-  ]
+variable "image_name" {
+  description = "OpenStack image name"
+  type        = string
+}
+
+variable "key_pair" {
+  description = "Name of the SSH key pair"
+  type        = string
+}
+
+variable "network_name" {
+  description = "Name of the network"
+  type        = string
+}
+
+variable "instance_metadata" {
+  description = "Metadata key-value pairs for the instance"
+  type        = map(string)
+  default     = {}
 }
