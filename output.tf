55cf4ef7f5502b1b9a1aab34f72e495a5d2ee739
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2026-02-26 01:51:07 +0000

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,9 +1,10 @@
 output "instance_id" {
-  value       = module.compute.instance_id
+  value       = module.users.instance_id
   description = "ID of the created OpenStack instance"
 }
 
 output "instance_ip" {
-  value       = module.compute.instance_ip
+  value       = module.users.instance_ip
   description = "Private IP address of the instance"
 }
+
