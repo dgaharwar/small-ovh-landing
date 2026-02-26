53a7d8bbd1198e8f976d2a66e284586b9f9d186a
Deepti Gaharwar <55699056+dgaharwar@users.noreply.github.com>
2026-02-26 00:38:02 +0000

    Delete output.tf

--- output.tf
+++
@@ -1,4 +0,0 @@
-output "image_name_to_id" {
-  description = "Map of image names to their OpenStack IDs"
-  value       = { for name, details in data.openstack_images_image_v2.images : name => details.id }
-}
